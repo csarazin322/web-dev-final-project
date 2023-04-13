59b2ce79e849607a4765185cede5cc7e69b8f711
Corey Sarazin <coreysarazin@gmail.com>
2023-04-13 12:59:20 -0400

    fix so bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
@@ -10,30 +9,11 @@ function App() {
     <BrowserRouter>
       <div className='container'>
         <Routes>
-          <Route path='/*' eleemnt={<Home />} />
+          <Route path='/*' element={<Home />} />
           <Route path='/profile' element={<Profile />} />
         </Routes>
       </div>
     </BrowserRouter>
-
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
