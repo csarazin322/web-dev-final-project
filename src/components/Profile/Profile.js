5f44c559191a90e99ee62abc499e9097600ac131
Corey Sarazin <coreysarazin@gmail.com>
2023-04-13 12:53:10 -0400

    add home and profile component starting files

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+const Profile = () => (
+  <div className={styles.Profile}>
+    Profile Component
+  </div>
+);
+
+Profile.propTypes = {};
+
+Profile.defaultProps = {};
+
+export default Profile;
