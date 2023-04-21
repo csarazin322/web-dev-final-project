8942f2b69ddc8d31014b23c7cd213b8500073e45
Corey Sarazin <coreysarazin@gmail.com>
2023-04-20 20:10:58 -0400

    make feed 3 wide

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,10 +2,22 @@ import React from 'react';
 import styles from './Feed.module.css';
 import Recipe from '../Recipe/Recipe';
 
+const listOfRecipes = ['weep', 'woo']
+
 const Feed = () => (
   <div className={styles.Feed}>
+    <div className='col=3 mt-3'>
+      {listOfRecipes.forEach((recepie) => {
+        return (
+          <div className='col-4 mb-2'>
+            <Recipe ></Recipe>
+          </div>
+        )
+      })}
+    </div>
+
     <div className='row mt-3'>
-      <div className='col-4 mb-2 mb-2'>
+      <div className='col-4 mb-2'>
         <Recipe></Recipe>
       </div>
       <div className='col-4 mb-2'>
@@ -24,7 +36,7 @@ const Feed = () => (
         <Recipe></Recipe>
       </div>
     </div>
-  </div>
+  </div >
 );
 
 Feed.propTypes = {};
