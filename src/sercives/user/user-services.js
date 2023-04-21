411865bf924477ac542a24f3180581f1b90f9564
aaronchisholm <84869779+aaronchisholm@users.noreply.github.com>
2023-04-21 18:52:43 -0400

    recipe services and thunks updates

--- src/sercives/user/user-services.js
+++ src/sercives/user/user-services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const USERS_REST_API_URL = process.env.REST_API_URL ?
     process.env.REST_API_URL + '/users'
-    : 'http://localhost:400/api/users';
+    : 'http://localhost:4000/api/users';
 
 const api = axios.create({
     withCredentials: true,
