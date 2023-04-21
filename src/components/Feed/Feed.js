2a2fdd6e19d4ffabcbf1f46c7eb71e07a4dad3fc
Corey Sarazin <coreysarazin@gmail.com>
2023-04-21 13:07:03 -0400

    automate recipe card generation with map and update spacing below

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,39 +2,18 @@ import React from 'react';
 import styles from './Feed.module.css';
 import Recipe from '../Recipe/Recipe';
 
-const listOfRecipes = ['weep', 'woo']
+const listOfRecipes = ['weep', 'woo', 'thing', 'it']
 
 const Feed = () => (
   <div className={styles.Feed}>
-    <div className='col=3 mt-3'>
-      {listOfRecipes.forEach((recepie) => {
+    <div className='row mt-3'>
+      {listOfRecipes.map((_) => {
         return (
-          <div className='col-4 mb-2'>
-            <Recipe ></Recipe>
+          <div className='col-4 mb-4'>
+            <Recipe></Recipe>
           </div>
         )
       })}
-    </div>
-
-    <div className='row mt-3'>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
-      <div className='col-4 mb-2'>
-        <Recipe></Recipe>
-      </div>
     </div>
   </div >
 );
