90de059397a8c2ae6be6812af38e1c4b20ea5a69
aaronchisholm <84869779+aaronchisholm@users.noreply.github.com>
2023-04-22 18:28:37 -0400

    Logic for like button

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,20 +2,29 @@ import React from 'react';
 import styles from './Recipe.module.css';
 import recipeImg from './recipe.png';
 import { useSelector } from 'react-redux';
-import { FontAwesome } from '@fortawesome/react-fontawesome'
+import { FontAwesome, FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import {updateUserThunk} from '../../sercives/user/user-thunks';
 import defaultRecipe from '../../data/default-recipe';
+import { Dispatch } from 'react';
 
 
 const Recipe = (recipe = defaultRecipe) => {
   
   const { currentUser } = useSelector((state) => state.users);
 
-  const likeRecipe= () => {
-    const updateLikedRecipesIds = currentUser.likedRecipesIds.push(recipe._id);
+  const likeRecipe = async () => {
+    const updateLikedRecipesIds = currentUser.likedRecipesIds;
+    updateLikedRecipesIds.push(recipe._id);
     const updatedUser = {... currentUser, likedRecipesIds: updateLikedRecipesIds};
-    const response = updateUserThunk(updatedUser);
+    const response = await dispatch(updateUserThunk(updatedUser));
+  }
+
+  const unlikeRecipe = async () => {
+    const updateLikedRecipesIds = currentUser.likedRecipesIds;
+    updateLikedRecipesIds.filter(recipe._id);
+    const updatedUser = {... currentUser, likedRecipesIds: updateLikedRecipesIds};
+    const response = await dispatch(updateUserThunk(updatedUser));
   }
   
   
@@ -25,15 +34,16 @@ const Recipe = (recipe = defaultRecipe) => {
       <img className='card-img-top' src={recipeImg} alt={recipeImg} />
       <div className='card-body'>
         <h5 className='card-title'>
-          ${recipe.title}
+          {recipe.title}
         </h5>
         <p className='card-text'>
-          ${recipe.description}
+          {recipe.description}
         </p>
-        ${currentUser ?
-          <FontAwesome onClick={ likeRecipe } icon={faHeart} color={currentUser.likedRecipesIds.find((recipeId) => recipe._id === recipeId) ? 'red' : ''}/>
-          :
-          ""}
+        {currentUser ?
+          (currentUser.likedRecipesIds.find((recipeId) => recipe._id === recipeId) ?
+          <FontAwesomeIcon onClick={ unlikeRecipe } icon={faHeart} color={'red'}/> :
+          <FontAwesomeIcon onClick={ likeRecipe } icon={faHeart}/>)
+          : ''}
 
       </div>
     </div>
