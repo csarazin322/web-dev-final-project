1a6167ad7d9a7f0800e635ec58d2f3a29875e611
Corey Sarazin <coreysarazin@gmail.com>
2023-04-22 18:34:53 -0400

    fixing recipe object type stuff before pulling red heard conditional changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import Recipe from '../Recipe/Recipe';
 import styles from './Home.module.css';
+import defaultRecipe from '../../data/default-recipe';
 
-const listOfRecipes = [];
+const listOfRecipes = ['wee'];
 
 const Home = () => (
   <div className={styles.Home}>
     <div className='row justify-content-start'>
-      <div className='col-8'>
+      <div className='col-8 mt-3'>
         {listOfRecipes.map((_) => {
           return (
             <div className='row'>
-              <Recipe></Recipe>
+              <Recipe recipe={defaultRecipe}></Recipe>
             </div>
           )
         })}
       </div>
-      <div className='col-4'>
+      <div className='col-4 mt-3'>
         popular chefs ... have follow button appear when logged in
       </div>
     </div>
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,44 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Recipe.module.css';
 import recipeImg from './recipe.png';
-import { useSelector } from 'react-redux';
-import { FontAwesome } from '@fortawesome/react-fontawesome'
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import {updateUserThunk} from '../../sercives/user/user-thunks';
+import { updateUserThunk } from '../../sercives/user/user-thunks';
 import defaultRecipe from '../../data/default-recipe';
+import defaultUser from '../../data/default-user';
 
 
-const Recipe = (recipe = defaultRecipe) => {
-  
+const Recipe = ({ recipe = defaultRecipe }) => {
+  console.log(`this is the recipe id ${recipe._id}`)
+  console.log(recipe)
+
   const { currentUser } = useSelector((state) => state.users);
+  const [user, setUser] = useState(defaultUser)
+  const dispatch = useDispatch()
 
-  const likeRecipe= () => {
-    const updateLikedRecipesIds = currentUser.likedRecipesIds.push(recipe._id);
-    const updatedUser = {... currentUser, likedRecipesIds: updateLikedRecipesIds};
-    const response = updateUserThunk(updatedUser);
+  const likeRecipe = async () => {
+    console.log(currentUser)
+    console.log(recipe._id)
+    const newLikedRecipes = [...currentUser.likedRecipesIds, recipe._id]
+
+    const updatedUser = { ...currentUser, likedRecipesIds: newLikedRecipes };
+    console.log(updatedUser)
+    const response = await dispatch(updateUserThunk(updatedUser));
   }
-  
-  
-  return (
-  <div className={styles.Recipe}>
-    <div className='card'>
-      <img className='card-img-top' src={recipeImg} alt={recipeImg} />
-      <div className='card-body'>
-        <h5 className='card-title'>
-          ${recipe.title}
-        </h5>
-        <p className='card-text'>
-          ${recipe.description}
-        </p>
-        ${currentUser ?
-          <FontAwesome onClick={ likeRecipe } icon={faHeart} color={currentUser.likedRecipesIds.find((recipeId) => recipe._id === recipeId) ? 'red' : ''}/>
-          :
-          ""}
 
+
+  return (
+    <div className={styles.Recipe}>
+      <div className='card'>
+        <img className='card-img-top' src={recipeImg} alt={recipeImg} />
+        <div className='card-body'>
+          <h5 className='card-title'>
+            {recipe.title}
+          </h5>
+          <p className='card-text'>
+            {recipe.description}
+          </p>
+
+          {currentUser ?
+            <FontAwesomeIcon onClick={likeRecipe} icon={faHeart} color={currentUser.likedRecipesIds.find((recipeId) => recipe._id === recipeId) ? 'red' : ''} />
+            :
+            ''}
+
+        </div>
       </div>
     </div>
-  </div>
-);}
+  );
+}
 
 Recipe.propTypes = {};
 
